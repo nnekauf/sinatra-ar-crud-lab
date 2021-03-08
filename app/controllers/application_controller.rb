0253b58db96abe1649663a327134e2e16c949bdc
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-07 20:24:30 -0500

    commit tree 34d6de353f1604c6ff31550c460334136b878b5f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # get '/' do
+  # end
+  
+  get '/articles' do
+    @articles = Article.all
+
+
+    erb :index
   end
-end
+
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+
+    erb :show
+  end
+
+ 
+
+  post '/articles' do
+  
+    @article = Article.create(params)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect to "/articles"
+  end
+end	  
